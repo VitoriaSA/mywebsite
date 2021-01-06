4e56b1f779a393a5c3820357eb6cc0b46e5ee992
vitorias <vitoriasimoes_2000@outlook.com>
2021-01-06 02:01:52 -0300

    Configuração do ambiente

---
+++ src/app/modules/home/components/home/home.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  name: string;
+
+  constructor() {
+    this.name = environment.name;
+  }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/modules/root/app.module.ts
+++ src/app/modules/root/app.module.ts
@@ -1,12 +1,15 @@
+import { HomeComponent } from '../home/components/home/home.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './component/app.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
